9a1424c61f473941cc20d51bf46faf3c3515b21f
Zahid Hasan <30589654+xahid007@users.noreply.github.com>
2026-04-18 13:46:58 +0000

    remove duplicates in array

---
+++ array/remove-duplicates.c
@@ -0,0 +1,35 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+    int arr[10] = {1,1,2,3,45,6,2,6,7,45};
+    int brr[10];
+    
+    int length  = sizeof(arr)/sizeof(arr[0]);
+    int count = 0;
+    
+    for (int i = 0; i<length;i++)
+    {
+        int flag = 0;
+        for (int j = 0; j< length; j++)
+        {
+            if(arr[i] == brr[j])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        
+        if(flag == 0)
+        {
+            brr[count] = arr[i];
+            count++;
+        }
+    }
+    
+    int l = sizeof(brr) / sizeof(brr[0]);
+    for (int i = 0; i<count; i++)
+        printf("%d,",brr[i]);
+ 
+}
