edfd0340e5d193dd8f169816b7bdeceda1eca507
Zahid Hasan <30589654+xahid007@users.noreply.github.com>
2026-04-20 03:33:46 +0000

    swap tow nunbers without comparision operator

---
+++ swap_2_num_wthout_comparision_op.c
@@ -0,0 +1,17 @@
+// swap numbers without using comparisoin operator
+#include <stdio.h>
+
+int main() 
+{
+    int a = 10;
+    int b = 20;
+    
+    printf("Before swap a = %d and b = %d\n", a,b);
+    
+    b = a+b;
+    a = b - a; //20
+    b = b - a;
+    
+    printf("After Swap a = %d and b =%d",a,b);
+
+}
